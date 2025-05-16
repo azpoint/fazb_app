267be5ef88b5642fb2403c291b35c958981d51a2
Alejandro Z <alejo8708@gmail.com>
2025-05-16 17:12:22 +0200

    empty-suite-image

--- app/panel/(suites)/page.jsx
+++ app/panel/(suites)/page.jsx
@@ -2,9 +2,9 @@ import prisma from "@/lib/prisma";
 
 //Components
 import SuiteCard from "@/src/components/panel/cards/SuiteCard";
+import Image from "next/image";
 
 export default async function Suites() {
-
 	const suites = await prisma.suite.findMany();
 
 	const firstImages = suites.map((suite) => {
@@ -17,22 +17,43 @@ export default async function Suites() {
 
 	return (
 		<div className="container mx-auto px-4 py-8">
-			<div className="text-[60px] font-bold text-center text-sky-800">Obras/Suites</div>
+			<div className="text-[60px] font-bold text-center text-sky-800">
+				Obras/Suites
+			</div>
 			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mt-8">
-				{suites.map((suite, index) => (
-					<SuiteCard
-						key={suite.suite_id}
-						image={firstImages[index]?.image}
-						imageDescription={firstImages[index]?.imageDescription}
-						title={suite.title}
-						createdAt={suite.created?.toLocaleDateString("es-ES", { year: "numeric" })}
-						revAt={suite.rev?.toLocaleDateString("es-ES", { year: "numeric" })}
-						publishedAt={suite.createdAt?.toLocaleDateString()}
-						lastUpdateAt={suite.updatedAt?.toLocaleDateString()}
-						published={suite.published}
-						slug={suite.slug}
-					/>
-				))}
+				{suites.length !== 0 ? (
+					suites.map((suite, index) => (
+						<SuiteCard
+							key={suite.suite_id}
+							image={firstImages[index]?.image}
+							imageDescription={
+								firstImages[index]?.imageDescription
+							}
+							title={suite.title}
+							createdAt={suite.created?.toLocaleDateString(
+								"es-ES",
+								{ year: "numeric" }
+							)}
+							revAt={suite.rev?.toLocaleDateString("es-ES", {
+								year: "numeric",
+							})}
+							publishedAt={suite.createdAt?.toLocaleDateString()}
+							lastUpdateAt={suite.updatedAt?.toLocaleDateString()}
+							published={suite.published}
+							slug={suite.slug}
+						/>
+					))
+				) : (
+					<div className="col-span-full flex justify-center bg-amber-700">
+						<Image
+							src={`/desert_plant.gif`}
+							width={800}
+							height={600}
+							alt="Empty space"
+							className=""
+						/>
+					</div>
+				)}
 			</div>
 		</div>
 	);
