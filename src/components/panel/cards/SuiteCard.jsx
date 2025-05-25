bf2e9d0b8b00acaf96f8fa5408687944e644ccf2
Alejandro Z <alejo8708@gmail.com>
2025-05-25 18:29:30 +0200

    suiteCard-published-state

--- src/components/panel/cards/SuiteCard.jsx
+++ src/components/panel/cards/SuiteCard.jsx
@@ -1,102 +1,114 @@
 import Image from "next/image";
 import Link from "next/link";
 import appPaths from "@/src/appPaths";
+import { revalidatePath } from "next/cache";
 
 //Componets
 import { FaCircle } from "react-icons/fa";
 import { MdEditDocument } from "react-icons/md";
 import { MdDelete } from "react-icons/md";
+import prisma from "@/lib/prisma";
 
 export default function SuiteCard({
-	image,
-	imageDescription,
-	title,
-	createdAt,
-	revAt,
-	publishedAt,
-	lastUpdateAt,
-	published,
-	suite_id,
+    image,
+    imageDescription,
+    title,
+    createdAt,
+    revAt,
+    publishedAt,
+    lastUpdateAt,
+    published,
+    suite_id,
 }) {
-	const imageCard = image || "/assets/Oraquesta-70s-fazb.jpg";
+    const imageCard = image || "/assets/Oraquesta-70s-fazb.jpg";
 
-	return (
-		<div className="bg-slate-200 rounded-xl overflow-hidden shadow-sky-950/50 shadow-xl hover:scale-102 transition-all duration-300">
-			<div className="relative aspect-[2/1.5]">
-				<Link href={""}>
-					<Image
-						src={imageCard}
-						alt={imageDescription || "Suite Text"}
-						fill={true}
-						className="object-cover object-center hover:brightness-125"
-					/>
-				</Link>
-			</div>
-			<div className="flex justify-evenly w-full my-5">
-				<Link href={appPaths.editSuite(suite_id)}>
-					<div className="relative" title="Editar Obra">
-						<MdEditDocument className="w-8 h-8 text-sky-700 cursor-pointer hover:text-sky-500 hover:scale-125 transition-all duration-300" />
-					</div>
-				</Link>
+    async function handlePublishedStatus() {
+        "use server";
+        await prisma.suite.update({
+            where: { suite_id },
+            data: { published: !published },
+        });
 
-				<Link href={""}>
-					<div
-						className="relative hover:scale-125 transition-all duration-300"
-						title="Poner/Quitar en linea"
-					>
-						{published ? (
-							<FaCircle className="w-8 h-8 text-emerald-700 ml-auto" />
-						) : (
-							<FaCircle className="w-8 h-8 text-rose-700 ml-auto" />
-						)}
-					</div>
-				</Link>
+        revalidatePath("/panel");
+    }
 
-				<Link href={appPaths.deleteSuite(suite_id)}>
-					<div className="relative" title="Eliminar Obra">
-						<MdDelete className="w-8 h-8 text-sky-700 cursor-pointer hover:text-rose-700 hover:scale-125 transition-all duration-300" />
-					</div>
-				</Link>
-			</div>
+    return (
+        <div className="bg-slate-200 rounded-xl overflow-hidden shadow-sky-950/50 shadow-xl hover:scale-102 transition-all duration-300">
+            <div className="relative aspect-[2/1.5]">
+                <Link href={""}>
+                    <Image
+                        src={imageCard}
+                        alt={imageDescription || "Suite Text"}
+                        fill={true}
+                        className="object-cover object-center hover:brightness-125"
+                    />
+                </Link>
+            </div>
+            <div className="flex justify-evenly w-full my-5">
+                <Link href={appPaths.editSuite(suite_id)}>
+                    <div className="relative" title="Editar Obra">
+                        <MdEditDocument className="w-8 h-8 text-sky-700 cursor-pointer hover:text-sky-500 hover:scale-125 transition-all duration-300" />
+                    </div>
+                </Link>
 
-			<div className="p-2 text-sky-800 hover:text-sky-600">
-				<Link href={""}>
-					<h4 className="text-center text-2xl font-bold">{title}</h4>
-				</Link>
-			</div>
+                <Link href={""} onClick={handlePublishedStatus}>
+                    <div
+                        className="relative hover:scale-125 transition-all duration-300"
+                        title="Poner/Quitar en linea"
+                    >
+                        {published ? (
+                            <FaCircle className="w-8 h-8 text-emerald-700 ml-auto" />
+                        ) : (
+                            <FaCircle className="w-8 h-8 text-rose-700 ml-auto" />
+                        )}
+                    </div>
+                </Link>
 
-			<div>
-				<div>
-					<p className="w-fit py-1 px-4 font-semibold text-sky-700">
-						Creada en: <strong>{createdAt}</strong>
-					</p>
-				</div>
-				<div>
-					<p className="w-fit py-1 px-4 font-semibold text-sky-700">
-						Revisada en: <strong>{revAt}</strong>
-					</p>
-				</div>
-				<div>
-					<p className="w-fit py-1 px-4 font-semibold text-sky-700">
-						En sistema desde: <strong>{publishedAt}</strong>
-					</p>
-				</div>
-				<div>
-					<p className="w-fit pt-1 pb-2 px-4 font-semibold text-sky-700">
-						Última Actualización: <strong>{lastUpdateAt}</strong>
-					</p>
-				</div>
+                <Link href={appPaths.deleteSuite(suite_id)}>
+                    <div className="relative" title="Eliminar Obra">
+                        <MdDelete className="w-8 h-8 text-sky-700 cursor-pointer hover:text-rose-700 hover:scale-125 transition-all duration-300" />
+                    </div>
+                </Link>
+            </div>
 
-				<div className="w-full pb-2 font-semibold flex items-center">
-					<span
-						className={`mx-auto 
+            <div className="p-2 text-sky-800 hover:text-sky-600">
+                <Link href={""}>
+                    <h4 className="text-center text-2xl font-bold">{title}</h4>
+                </Link>
+            </div>
+
+            <div>
+                <div>
+                    <p className="w-fit py-1 px-4 font-semibold text-sky-700">
+                        Creada en: <strong>{createdAt}</strong>
+                    </p>
+                </div>
+                <div>
+                    <p className="w-fit py-1 px-4 font-semibold text-sky-700">
+                        Revisada en: <strong>{revAt}</strong>
+                    </p>
+                </div>
+                <div>
+                    <p className="w-fit py-1 px-4 font-semibold text-sky-700">
+                        En sistema desde: <strong>{publishedAt}</strong>
+                    </p>
+                </div>
+                <div>
+                    <p className="w-fit pt-1 pb-2 px-4 font-semibold text-sky-700">
+                        Última Actualización: <strong>{lastUpdateAt}</strong>
+                    </p>
+                </div>
+
+                <div className="w-full pb-2 font-semibold flex items-center">
+                    <span
+                        className={`mx-auto 
 							${published ? "text-green-500" : "text-rose-700"}
 						`}
-					>
-						En linea: {published ? "Si" : "No"}
-					</span>
-				</div>
-			</div>
-		</div>
-	);
+                    >
+                        En linea: {published ? "Si" : "No"}
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
 }
