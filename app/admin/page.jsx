59f6b3f6ca1b297304c0dd7c746a4a16e0cdaf8e
Alejandro Z <alejo8708@gmail.com>
2025-06-08 01:48:52 +0200

    login-session-redirect

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,12 +1,18 @@
 //Components
+import { getUserFromSession } from "@/lib/auth";
 import SignInForm from "@/src/components/panel/forms/SignInForm";
 import Footer from "@/src/components/web/Footer";
+import { redirect } from "next/navigation";
 
 export const metadata = {
 	title: 'Sign In'
 }
 
-export default function SignInPage() {
+export default async function SignInPage() {
+	const sessionPayload = await getUserFromSession()
+
+	if(sessionPayload) redirect('/admin/panel')
+
     return (
         <>
             <div className="flex-1 bg-slate-100 flex justify-center mt-[20vh]">
