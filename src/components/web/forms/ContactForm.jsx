5387f1602c50ecb2dd898a314d7ae0f99a92113e
Alejandro Z <alejo8708@gmail.com>
2025-06-16 15:40:18 +0200

    contactPage/mailgun-update

--- src/components/web/forms/ContactForm.jsx
+++ src/components/web/forms/ContactForm.jsx
@@ -7,7 +7,7 @@ import { useActionState, useState } from "react";
 import contactAction from "@/src/components/web/forms/actions/contact";
 
 export default function ContactForm() {
-		const [displayTrigger, setDisplayTrigger] = useState("");	
+    const [displayTrigger, setDisplayTrigger] = useState("");
     const [contactFormState, handleSubmit] = useActionState(contactAction, {
         success: null,
         error: null,
@@ -22,37 +22,46 @@ export default function ContactForm() {
                     Contacto
                 </h1>
 
-                <h2 className="text-2xl font-semibold text-sky-900">
+                {/* <h2 className="text-2xl font-semibold text-sky-900">
                     Escribe tu mensaje aquí:
-                </h2>
+                </h2> */}
 
                 <form action={handleSubmit}>
-					<div className="mt-4">
-                    <label htmlFor="emailField" className="text-xl font-semibold text-sky-900">
-                        Email:
-                    </label>
-                    <input
-                        type="email"
-                        name="email"
-                        id="emailField"
-                        className="h-[calc(2.5rem+8px)] mt-2 px-4 rounded w-full bg-slate-200 text-gray-800 placeholder:text-slate-400 focus:border-teal-500 focus:outline-none focus:border-b-8 transition-all duration-200"
-                        placeholder="tuemail@eldominio.com"
-                    />
-					
-                </div>
-                    <textarea
-                        name="message"
-                        className="mt-4 h-[40vh] w-full px-8 py-4 rounded-md bg-slate-200 border-0 outline-none text-sky-950 caret-sky-700 focus:border-teal-500 focus:outline-none focus:border-b-8 transition-all duration-200"
-                    ></textarea>
-					<button
-                type="submit"
-                className={`bg-teal-700 w-2/5 mx-auto block mt-14 p-4 text-slate-200 text-2xl font-semibold rounded-md hover:bg-teal-600 cursor-pointer transition-colors duration-200`}
-            >
-                Enviar
-            </button>
+                    <div className="mt-4">
+                        <label
+                            htmlFor="emailField"
+                            className="text-xl font-semibold text-sky-900"
+                        >
+                            Tú email para responderte:
+                        </label>
+                        <input
+                            type="email"
+                            name="email"
+                            id="emailField"
+                            className="h-[calc(2.5rem+8px)] mt-2 px-4 rounded w-full bg-slate-200 text-gray-800 placeholder:text-slate-400 focus:border-teal-500 focus:outline-none focus:border-b-8 transition-all duration-200"
+                            placeholder="tuemail@eldominio.com"
+                        />
+                    </div>
+                    <div className="mt-4">
+                        <label
+                            htmlFor="emailField"
+                            className="text-xl font-semibold text-sky-900 "
+                        >
+                            Escribe tu mensaje aquí:
+                        </label>
+                        <textarea
+                            name="message"
+                            className="mt-4 h-[40vh] w-full px-8 py-4 rounded-md bg-slate-200 border-0 outline-none text-sky-950 caret-sky-700 focus:border-teal-500 focus:outline-none focus:border-b-8 transition-all duration-200"
+                        ></textarea>
+                    </div>
+                    <button
+                        type="submit"
+                        className={`bg-teal-700 w-2/5 mx-auto block mt-14 p-4 text-slate-200 text-2xl font-semibold rounded-md hover:bg-teal-600 cursor-pointer transition-colors duration-200`}
+                    >
+                        Enviar
+                    </button>
                 </form>
             </div>
         </>
     );
 }
-
