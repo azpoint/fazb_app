b5d873e1032fa352215ef25590a9651f812e0fbe
Alejandro Z <alejo8708@gmail.com>
2025-07-28 18:21:15 +0200

    prismSchema-order/deploy

--- swarm/get_script.sh
+++ swarm/get_script.sh
@@ -6,3 +6,7 @@ BASE_URL="https://raw.githubusercontent.com/azpoint/fazb_app/refs/heads/main/swa
 curl -fsSL -o  "$SCRIPT_OUT" "$BASE_URL/full_deploy.sh" \
  && echo "✅ Updated: full_deploy.sh" \
  || echo "❌ Failed to update full_deploy.sh"
+
+curl -fsSL -o ~/sites/fazb-app/stacks/fazb_deploy.yml "$BASE_URL/fazb_deploy.yml" \
+  && echo "✅ Updated: fazb_deploy.yml" \
+  || echo "❌ Failed to update fazb_deploy.yml"
