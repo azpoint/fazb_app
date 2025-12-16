4a06a4a47344594d8fda6dfa060153ce960752bd
Alejandro Z <alejo8708@gmail.com>
2025-12-17 00:46:42 +0100

    dockefile_user_fix

---
+++ ci-cd/push_nginx_conf.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rsync -avh /home/alejo/dev/projects/fazb/fazb_app/swarm/nginx.conf root_main_vps:/etc/nginx/nginx.conf
---
+++ ci-cd/push_swarm_deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rsync -avh /home/alejo/dev/projects/fazb/fazb_app/swarm/fazb_deploy deploy_main_vps:/home/deploy/sites/fazb_app
