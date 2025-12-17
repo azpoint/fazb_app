1f2a7eb3d0dfe91b3afe498ca443ad8e050c72f6
Alejandro Z <alejo8708@gmail.com>
2025-12-17 20:01:32 +0100

    v1.2.4 deploy done

--- ci-cd/push_nginx_conf.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rsync -avh /home/alejo/dev/projects/fazb/fazb_app/swarm/nginx.conf root_main_vps:/etc/nginx/nginx.conf
--- ci-cd/push_swarm_deploy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rsync -avh /home/alejo/dev/projects/fazb/fazb_app/swarm/fazb_deploy deploy_main_vps:/home/deploy/sites/fazb_app
