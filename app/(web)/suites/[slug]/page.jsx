93c562fd89df1b22cbf47c0745180ec8bdd1bec7
Alejandro Z <alejo8708@gmail.com>
2026-04-06 12:29:37 +0200

    deploy file fix

--- app/(web)/suites/[slug]/page.jsx
+++ app/(web)/suites/[slug]/page.jsx
@@ -11,156 +11,152 @@ import { GrPrevious } from "react-icons/gr";
 import Image from "next/image";
 
 export async function generateMetadata({ searchParams }) {
-    const params = await searchParams;
-    return {
-        title: params.title,
-    };
+  const params = await searchParams;
+  return {
+    title: params.title,
+  };
 }
 
 export default async function SuitePage({ params }) {
-    const { slug } = await params;
+  const { slug } = await params;
 
-    const suite = await prisma.suite.findUnique({
-        where: { slug },
-    });
+  const suite = await prisma.suite.findUnique({
+    where: { slug },
+  });
 
-    //Redirects if no suite or unpublished
-    if (!suite || suite.published === false) redirect(appPaths.suites());
+  //Redirects if no suite or unpublished
+  if (!suite || suite.published === false) redirect(appPaths.suites());
 
-    const HTML = marked.parse(suite.notes ? suite.notes : "");
+  const HTML = marked.parse(suite.notes ? suite.notes : "");
 
-    const images = JSON.parse(suite.images)?.map((item) => item.filePath);
+  const images = JSON.parse(suite.images)?.map((item) => item.filePath);
 
-    const audios = JSON.parse(suite.audios)?.map((item) => item.filePath);
-    const audiosDescription = JSON.parse(suite.audios)?.map(
-        (item) => item.fileDescription
-    );
-    const ytLinks = JSON.parse(suite.ytLinks);
-    const movs = JSON.parse(suite.mov);
+  const audios = JSON.parse(suite.audios)?.map((item) => item.filePath);
+  const audiosDescription = JSON.parse(suite.audios)?.map(
+    (item) => item.fileDescription,
+  );
+  const ytLinks = JSON.parse(suite.ytLinks);
+  const movs = JSON.parse(suite.mov);
 
-    return (
-        <>
-            <div className="w-full h-24" />
+  return (
+    <>
+      <div className="w-full h-24" />
 
-            <div className="text-sky-900 font-bold flex justify-end max-w-screen-lg mx-auto my-8">
-                <Link
-                    href="/suites"
-                    className="group inline-flex items-center text-2xl md:text-4xl no-underline transition-transform duration-300 hover:scale-105"
-                >
-                    <GrPrevious className="size-8 md:size-10  text-sky-900 group-hover:text-sky-600 transition-colors duration-300" />
-                    <span className="ml-2 text-sky-900 group-hover:text-sky-600 transition-colors duration-300">
-                        Volver
-                    </span>
-                </Link>
-            </div>
+      <div className="text-sky-900 font-bold flex justify-end max-w-screen-lg mx-auto my-8">
+        <Link
+          href="/suites"
+          className="group inline-flex items-center text-2xl md:text-4xl no-underline transition-transform duration-300 hover:scale-105"
+        >
+          <GrPrevious className="size-8 md:size-10  text-sky-900 group-hover:text-sky-600 transition-colors duration-300" />
+          <span className="ml-2 text-sky-900 group-hover:text-sky-600 transition-colors duration-300">
+            Volver
+          </span>
+        </Link>
+      </div>
 
-            <h1 className="mt-8 text-sky-900 text-4xl text-center md:text-6xl font-bold">
-                {suite.title}
-            </h1>
+      <h1 className="mt-8 text-sky-900 text-4xl text-center md:text-6xl font-bold">
+        {suite.title}
+      </h1>
 
-            <div className="font-bold flex flex-col max-w-screen-lg mx-auto mt-12">
-                <h3 className="text-xl md:text-2xl">
-                    Composición:{" "}
-                    {suite.composedInit
-                        ? `${suite.composedInit} - ${suite.composed}`
-                        : suite.composed}
-                </h3>
-            </div>
+      <div className="font-bold flex flex-col max-w-screen-lg mx-auto mt-12">
+        <h3 className="text-xl md:text-2xl">
+          Composición:{" "}
+          {suite.composedInit
+            ? `${suite.composedInit} - ${suite.composed}`
+            : suite.composed}
+        </h3>
+      </div>
 
-            {suite.rev ? (
-                <div className="font-bold flex flex-col max-w-screen-lg mx-auto mt-4">
-                    <h3 className="text-xl md:text-2xl">
-                        Última revisión: {suite.rev}
-                    </h3>
-                </div>
-            ) : null}
+      {suite.rev ? (
+        <div className="font-bold flex flex-col max-w-screen-lg mx-auto mt-4">
+          <h3 className="text-xl md:text-2xl">Última revisión: {suite.rev}</h3>
+        </div>
+      ) : null}
 
-            {suite.timeLength ? (
-                <div className="font-bold flex flex-col max-w-screen-lg mx-auto mt-4">
-                    <h3 className="text-xl md:text-2xl">
-                        Duración: ~{suite.timeLength}
-                    </h3>
-                </div>
-            ) : null}
+      {suite.timeLength ? (
+        <div className="font-bold flex flex-col max-w-screen-lg mx-auto mt-4">
+          <h3 className="text-xl md:text-2xl">Duración: ~{suite.timeLength}</h3>
+        </div>
+      ) : null}
 
-            {movs ? (
-                <div className="font-bold flex flex-col max-w-screen-lg mx-auto mt-8">
-                    <h2 className="text-2xl md:text-4xl">Movimientos</h2>
-                    <ul className="mt-4 ml-8 list-disc list-inside">
-                        {movs.map((mov, index) => (
-                            <li key={"mov" + index} className="mt-2 text-xl">
-                                {mov}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            ) : null}
+      {movs ? (
+        <div className="font-bold flex flex-col max-w-screen-lg mx-auto mt-8">
+          <h2 className="text-2xl md:text-4xl">Movimientos</h2>
+          <ul className="mt-4 ml-8 list-disc list-inside">
+            {movs.map((mov, index) => (
+              <li key={"mov" + index} className="mt-2 text-xl">
+                {mov}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : null}
 
-            <hr className="my-12 w-full max-w-screen-lg mx-auto text-gray-300" />
+      <hr className="my-12 w-full max-w-screen-lg mx-auto text-gray-300" />
 
-            <article
-                dangerouslySetInnerHTML={{ __html: HTML }}
-                className="prose prose-slate prose-xl mx-auto w-full max-w-screen-lg"
-            ></article>
+      <article
+        dangerouslySetInnerHTML={{ __html: HTML }}
+        className="prose prose-slate prose-xl mx-auto w-full max-w-screen-lg"
+      ></article>
 
-            {images && images.length === 1 ? (
-                <div className="mx-auto max-w-screen-lg mt-12 relative aspect-video overflow-hidden rounded-sm">
-                    <Image
-                        src={`/api${images[0]}`}
-                        fill
-                        className="object-contain object-center"
-                        alt="Image"
-                    />
-                </div>
-            ) : images && images.length > 1 ? (
-                <EmblaCarousel images={images} />
-            ) : null}
+      {images && images.length === 1 ? (
+        <div className="mx-auto max-w-screen-lg mt-12 relative aspect-video overflow-hidden rounded-sm">
+          <Image
+            src={`/api${images[0]}`}
+            fill
+            className="object-contain object-center"
+            alt="Image"
+          />
+        </div>
+      ) : images && images.length > 1 ? (
+        <EmblaCarousel images={images} />
+      ) : null}
 
-            <div className="max-w-screen-lg my-12 w-full mx-auto space-y-8">
-                {audios
-                    ? audios.map((audio, index) => (
-                          <AudioCardWeb
-                              key={"audio_car_web" + index}
-                              audio={audio}
-                              description={audiosDescription[index]}
-                          />
-                      ))
-                    : null}
-            </div>
+      <div className="max-w-screen-lg my-12 w-full mx-auto space-y-8">
+        {audios
+          ? audios.map((audio, index) => (
+              <AudioCardWeb
+                key={"audio_car_web" + index}
+                audio={audio}
+                description={audiosDescription[index]}
+              />
+            ))
+          : null}
+      </div>
 
-            <div className="my-16 space-y-4 max-w-screen-lg mx-auto">
-                {ytLinks
-                    ? ytLinks.map((video, index) => (
-                          <div
-                              className="w-full aspect-video overflow-hidden rounded-md"
-                              key={`ytVideo-${index}`}
-                          >
-                              <iframe
-                                  loading="lazy"
-                                  className="w-full h-full"
-                                  src={`https://www.youtube.com/embed/${video}`}
-                                  title="YouTube video player"
-                                  frameBorder="0"
-                                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                  referrerPolicy="strict-origin-when-cross-origin"
-                                  allowFullScreen
-                              ></iframe>
-                          </div>
-                      ))
-                    : null}
-            </div>
+      <div className="my-16 space-y-4 max-w-screen-lg mx-auto">
+        {ytLinks
+          ? ytLinks.map((video, index) => (
+              <div
+                className="w-full aspect-video overflow-hidden rounded-md"
+                key={`ytVideo-${index}`}
+              >
+                <iframe
+                  loading="lazy"
+                  className="w-full h-full"
+                  src={`https://www.youtube.com/embed/${video}`}
+                  title="YouTube video player"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerPolicy="strict-origin-when-cross-origin"
+                  allowFullScreen
+                ></iframe>
+              </div>
+            ))
+          : null}
+      </div>
 
-            <div className="text-sky-900 font-bold flex justify-end max-w-screen-lg mx-auto my-8">
-                <Link
-                    href="/suites"
-                    className="group inline-flex items-center text-2xl md:text-4xl no-underline transition-transform duration-300 hover:scale-105"
-                >
-                    <GrPrevious className="size-8 md:size-10  text-sky-900 group-hover:text-sky-600 transition-colors duration-300" />
-                    <span className="ml-2 text-sky-900 group-hover:text-sky-600 transition-colors duration-300">
-                        Volver
-                    </span>
-                </Link>
-            </div>
-        </>
-    );
+      <div className="text-sky-900 font-bold flex justify-end max-w-screen-lg mx-auto my-8">
+        <Link
+          href="/suites"
+          className="group inline-flex items-center text-2xl md:text-4xl no-underline transition-transform duration-300 hover:scale-105"
+        >
+          <GrPrevious className="size-8 md:size-10  text-sky-900 group-hover:text-sky-600 transition-colors duration-300" />
+          <span className="ml-2 text-sky-900 group-hover:text-sky-600 transition-colors duration-300">
+            Volver
+          </span>
+        </Link>
+      </div>
+    </>
+  );
 }
--- app/layout.jsx
+++ app/layout.jsx
@@ -3,24 +3,24 @@ import "@/app/globals.css";
 import { openSans, dancingScript } from "@/src/styles/fonts";
 
 export const metadata = {
-    title: {
-        default: "Francisco Zapata Bello",
-        template: "%s | Francisco Zapata Bello",
-    },
-    description: "Fracisco Zapata Augusto Bello, compositor, director y vida",
+  title: {
+    default: "Francisco Zapata Bello",
+    template: "%s | Francisco Zapata Bello",
+  },
+  description: "Fracisco Zapata Augusto Bello, compositor, director y vida",
 };
 
 export default function RootLayout({ children }) {
-    return (
-        <html
-            lang="en"
-            className={`${openSans.variable} ${dancingScript.variable} !scroll-smooth h-full`}
-        >
-            <body className="flex flex-col min-h-screen bg-slate-100">
-                {children}
-                {/* This is the div where your portals will render */}
-                <div id="portal-root"></div>
-            </body>
-        </html>
-    );
+  return (
+    <html
+      lang="en"
+      className={`${openSans.variable} ${dancingScript.variable} !scroll-smooth h-full`}
+    >
+      <body className="flex flex-col min-h-screen bg-slate-100">
+        {children}
+        {/* This is the div where your portals will render */}
+        <div id="portal-root"></div>
+      </body>
+    </html>
+  );
 }
